790ba6f4759c5ce9a39fc9f98d699849cd2a9be2
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-02 01:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function cntLevel() {
+	const level = document.getElementById('level');
+	let res = '';
+	for (let i = 0; i < level.length; i++) {
+		if (level[i] > 0) {
+			res += level[i];
+			
+		}
+		alert("The level of the element is :" res);
+	}
+}
